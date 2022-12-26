7a76b0ed118045368721871dd7f6108365feb1ff
David Zeman <david.zeman@outlook.com>
2022-12-27 00:31:54 +0100

    v

--- trp_printer.py
+++ trp_printer.py
@@ -6,6 +6,8 @@ def to_console(trp : TRP, name = 'TRP', stats = None):
     print(f'======== {name} at {trp.minutes} ========')
     for request in trp.requests:
         print(f'Request {format_tuple(request.nodeFrom)} -> {format_tuple(request.nodeTo)} | {request.twFrom:.2f} - {request.twTo:.2f}')
+    for vehicle in trp.vehicles:
+        print(f'Vehicle {vehicle.id} at {vehicle.node}')
     for route in trp.routes:
         print(f'Route {[str(format_tuple(route_point.node)) + " at " + str(route_point.minutes) for route_point in route]}')
     if stats:
