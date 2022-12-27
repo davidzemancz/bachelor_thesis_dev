eadf448d5b6023262c0edf3fe912bfc86b4f7cfa
David Zeman <david.zeman@outlook.com>
2022-12-27 22:10:30 +0100

    alg

---
+++ trp_alg.py
@@ -0,0 +1,23 @@
+
+def recive_requests(trp):
+    pass
+
+def plan_routes(trp):
+    pass
+
+def close_requests(trp):
+    pass
+
+def update_vehicles(trp):
+
+def main():
+    trp = {}
+    while True:
+        recive_requests(trp)
+        plan_routes(trp)
+        close_requests(trp)
+        update_vehicles(trp)
+
+
+if __name__ == "__main__":
+    main()
